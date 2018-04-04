64b4a54d5da30910adb9badd550e9a5ab250d90d
hossam <eng.hsamir@gmail.com>
2018-04-04 17:04:51 +0200

    fix local database connection

--- routes.py
+++ routes.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request
-from models import db
+from flask import Flask, render_template, request, session, url_for, redirect   
+from models import db,User
 from forms import SignupForm
 
 app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://localhost/learningflask'
+# app.config['SQLALCHEMY_DATABASE_URI']='postgresql://localhost/learningflask'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://user_1:TEST123@localhost/learningflask'
 db.init_app(app)
 
 app.secret_key="development-key"
@@ -23,9 +24,17 @@ def signup():
         if form.validate()==False:
             return render_template('signup.html',form=form)
         else:
-            return "Success!"
+            newuser=User(form.first_name.data,form.last_name.data,form.email.data,form.password.data)
+            db.session.add(newuser)
+            db.session.commit()
+
+            session['email']=newuser.email
+            return redirect(url_for('home'))
     elif request.method=="GET":
         return render_template('signup.html',form=form)
+@app.route("/home")
+def home():
+    return render_template("home.html")
 
 if __name__=="__main__":
     app.run(debug=True)
